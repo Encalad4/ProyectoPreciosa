dc025966bffa44e1d67d454d21220e828ac7f36b
Sebastian Encalada <encalada.sebastian03@gmail.com>
2025-02-01 20:48:21 -0500

    #2.0 update

--- js/script.js
+++ js/script.js
@@ -2,12 +2,44 @@ document.getElementById("submitButton").addEventListener("click", function() {
     const password = document.getElementById("passwordInput").value;
     const errorMessage = document.getElementById("errorMessage");
 
-    // Replace "ILoveYou" with your desired password
-    if (password === "ILoveYou") {
-        // Redirect to the main content page
-        window.location.href = "main_content.html";
+   
+    if (password === "1805461595") {
+        errorMessage.classList.add("hidden");
+        const correctSound = new Audio("../media/correctSound.mp3");
+        correctSound.play(); 
+        const yipiSound = new Audio("../media/Yipi.mp3");
+        yipiSound.play();   
+        confetti({
+            particleCount: 100, 
+            spread: 70, 
+            origin: { y: 0.6 } 
+        });
+        setTimeout(function() {
+            window.location.href = "main_content.html";
+        }, 2500);
+
     } else {
-        // Show error message
+
         errorMessage.classList.remove("hidden");
+        const errorSound = new Audio("../media/errorSound.wav");
+        errorSound.play();
     }
 });
+
+function createHearts() {
+    const numHearts = 20; 
+    const container = document.body;
+
+    for (let i = 0; i < numHearts; i++) {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+
+        
+        heart.style.left = `${Math.random() * 100}vw`;
+        heart.style.animationDelay = `${Math.random() * 5}s`;
+
+        container.appendChild(heart);
+    }
+}
+
+createHearts(); 
